ad21e3833a5531ddd89ebc7411b551efe86cfb0b
Antonio <antoniodawgc@gmail.com>
2026-02-22 11:31:06 +0000

    Creacion Enum

---
+++ src/Model/Genero.java
@@ -0,0 +1,15 @@
+package Model;
+
+public enum Genero {
+    NOVELA,
+    CIENCIA_FICCION,
+    HISTORIA,
+    FANTASIA,
+    MISTERIO,
+    INFANTIL,
+    POESIA,
+    TERROR,
+    NOVELA_NEGRA,
+    SUSPENSE,
+    OTRO
+}
