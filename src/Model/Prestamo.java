35c7b9d0d4da4020877a184bcae5c16672cc880c
joselu186 <joseluusasan@gmail.com>
2026-02-22 12:21:48 +0000

    Creacion clase prestamo

---
+++ src/Model/Prestamo.java
@@ -0,0 +1,53 @@
+package Model;
+
+import java.time.LocalDate;
+
+public class Prestamo {
+
+    private Libro libro;
+    private Usuario usuario;
+    private LocalDate fechaPrestamo;
+    private LocalDate fechaVencimiento;
+    private LocalDate fechaDevolucion;
+
+    private static final int DIAS_PRESTAMO = 30;
+
+    public Prestamo(Libro libro, Usuario usuario) {
+        this.libro = libro;
+        this.usuario = usuario;
+        this.fechaPrestamo = LocalDate.now();
+        this.fechaVencimiento = fechaPrestamo.plusDays(DIAS_PRESTAMO);
+    }
+
+    public void devolver() {
+        this.fechaDevolucion = LocalDate.now();
+    }
+
+    public boolean estaDevuelto() {
+        return fechaDevolucion != null;
+    }
+
+    public boolean estaVencido() {
+        return LocalDate.now().isAfter(fechaVencimiento) && !estaDevuelto();
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public LocalDate getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public LocalDate getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public LocalDate getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+}
